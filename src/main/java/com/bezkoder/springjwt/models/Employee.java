91a030eed00135e60852ce087bc66fd64dbaf99b
Naresh <naresh.gattu@thomsonreuters.com>
2020-07-01 13:12:37 +0530

    Initial commit

---
+++ src/main/java/com/bezkoder/springjwt/models/Employee.java
@@ -0,0 +1,21 @@
+package com.bezkoder.springjwt.models;
+
+public class Employee {
+	
+	private String Name;
+	private String age;
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+
+}
